1cf9f2b50a6d14c13e957676de38fac62c2883ee
Liz <31071746+caltrop2075@users.noreply.github.com>
2022-11-01 17:26:49 -0400

    Update fat-tre.sh

--- fat-tre.sh
+++ fat-tre.sh
@@ -2,10 +2,18 @@
 #------------------------------------------------------------------------------#
 #                            Programmed By Liz                                 #
 #------------------------------------------------------------------------------#
-title-80.sh -t line "fat-tre-*.dat brief FAT tree | alias fatree='fat-tre.sh'"
-sleep 1
-for fil in ~/data/fat-tre-*
+# fat-cat tree listing
+# modified to compact
+# add sed to remove special chars sol 'column' works
+#
+title-80.sh -t line "Brief FAT Tree\nexcluding Jerry's Music"
+out="$HOME/temp/temp.dat"
+echo -n > "$out"
+
+find ~/data -type f \! -name "fat-tre-16GB-1.dat" -name "fat-tre-*" | sort |
+while read fil
 do
-   echo
-   cat $fil
+   cat $fil | fat-tre.sed >> "$out"
+   echo >> "$out"
 done
+cat ~/temp/temp.dat | column -e
