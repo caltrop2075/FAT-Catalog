7c12fc3837f014a09e3ed0eaa9e784fe37d9b82a
Liz <31071746+caltrop2075@users.noreply.github.com>
2022-11-01 17:25:31 -0400

    Update fat-cat.sh

--- fat-cat.sh
+++ fat-cat.sh
@@ -7,7 +7,7 @@
 clear
 source ~/data/global.dat
 
-title-80.sh -t line "FAT Flash Drive Catalog: fat-cat.dat\ndelete empty directories"
+title-80.sh -t line "FAT Flash Drive Catalog: fat-cat.dat"
 sleep $(echo "scale=3; $dly / 2" | bc)
 
 #echo -e "\n$div_d\nre-mouting android\n$div_s"
@@ -17,6 +17,13 @@ sleep $(echo "scale=3; $dly / 2" | bc)
 #str=$(mnta.sh)
 #printf "%9s: %s\n" $str
 
+if [[ "$1" == "-h" || "$1" == "--help" ]]
+then
+   echo "usage: fat-cat.sh [-t -h]"
+   echo "       no option  process FAT drives"
+   echo "       -c         catalog only"
+   echo "       -h         help"
+fi
 f=0
 unset LC_ALL
 
@@ -25,22 +32,25 @@ do
    typ=$(findmnt -no fstype "$tgt")
    if [ $typ = "vfat" ] || [ $typ = "exfat" ]
    then
-      find $tgt -mindepth 1 -type d -empty -delete       # remove empty dirs
-      f=$((f+=1))
-      drv=${tgt##*/}                                     # get drv name w/o pth
       dev=$(findmnt -no source $tgt)
-      use=$(findmnt -o used,avail,size $tgt)             # drive stats
       echo -e "\n$div_d\n$dev $tgt $HOME/data/fat-cat-$drv.dat\n$div_s"
-      echo -e "$use\n" > "$HOME/data/fat-cat-$drv.dat"   # erase output file
-
-      mnt.sh -u $dev
-      sudo fatsort $dev
-      mnt.sh $dev
-                                                         # short tree script
-      echo -e "\n$div_s\n     creating catalog for: $tgt"
-      tree.sh $tgt >> "$HOME/data/fat-cat-$drv.dat"
-                                                         # fat tree
-      tree -nd --noreport $tgt > "$HOME/data/fat-tre-$drv.dat"
+      ((f++))
+      drv=${tgt##*/}                                     # get drv name w/o pth
+      if [[ $1 != "-c" ]]
+      then
+         echo "delete empty directories"
+         find $tgt -mindepth 1 -type d -empty -delete    # remove empty dirs
+         mnt.sh -u $dev                                  # sort
+         sudo fatsort $dev
+         mnt.sh $dev
+      else
+         echo "no sort - catalog only"
+      fi
+      echo -e "\n$div_s\ncreating catalog for: $tgt"
+      use=$(findmnt -o used,avail,size $tgt)             # drive stats
+      echo -e "$use\n" > "$HOME/data/fat-cat-$drv.dat"   # initialize output file
+      tree.sh $tgt >> "$HOME/data/fat-cat-$drv.dat"      # fat tree
+      tree.sh -nd --noreport $tgt > "$HOME/data/fat-tre-$drv.dat" # short tree
    fi
 done
 
